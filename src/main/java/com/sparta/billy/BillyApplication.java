f6a8533ad61f90eae784b1fd7ac4753b657d37b6
keembogeul <318qhtmf@naver.com>
2022-08-29 09:32:50 +0900

    feat : 초기 커밋

---
+++ src/main/java/com/sparta/billy/BillyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.billy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/billy/BillyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.billy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
