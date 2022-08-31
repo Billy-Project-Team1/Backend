6c9a37db28d3da05130630f7e6ab16954b70547b
keembogeul <318qhtmf@naver.com>
2022-08-31 20:40:26 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/sparta/billy/repository/MemberRepository.java
+++ src/main/java/com/sparta/billy/repository/MemberRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+    Optional<Member> findById(Long memberId);
     Long countByEmail(String email);
 }
