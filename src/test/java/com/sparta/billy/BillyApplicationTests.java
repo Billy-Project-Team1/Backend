ff906ed1201cd47231fa0d7f1785c02676be5923
ryujiwoo184 <83330576+ryujiwoo184@users.noreply.github.com>
2022-09-02 16:15:05 +0900

    Update BillyApplicationTests.java
    무중단 배포 테스트

--- src/test/java/com/sparta/billy/BillyApplicationTests.java
+++ src/test/java/com/sparta/billy/BillyApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.billy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BillyApplicationTests {
 
     @Test
