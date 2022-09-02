851a41112dd6e6618ac2e3a95cb2f04b77f5848d
ryujiwoo184 <83330576+ryujiwoo184@users.noreply.github.com>
2022-09-02 23:06:06 +0900

    Update BillyApplicationTests.java

--- src/test/java/com/sparta/billy/BillyApplicationTests.java
+++ src/test/java/com/sparta/billy/BillyApplicationTests.java
@@ -3,10 +3,10 @@ package com.sparta.billy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
+@SpringBootTest
 class BillyApplicationTests {
 
-//     @Test
+    @Test
     void contextLoads() {
     }
 
