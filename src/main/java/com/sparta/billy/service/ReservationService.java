413e3d1fd52542762f3ce173f8e8c418d6de9465
keembogeul <318qhtmf@naver.com>
2022-09-07 20:52:51 +0900

    📝 feat: Reservation 테이블과 ReservationDate 테이블 분리

--- src/main/java/com/sparta/billy/service/ReservationService.java
+++ src/main/java/com/sparta/billy/service/ReservationService.java
@@ -5,9 +5,8 @@ import com.sparta.billy.dto.ReservationDto.ReservationRequestDto;
 import com.sparta.billy.dto.ReservationDto.ReservationStateRequestDto;
 import com.sparta.billy.dto.ResponseDto;
 import com.sparta.billy.dto.SuccessDto;
-import com.sparta.billy.model.Member;
-import com.sparta.billy.model.Post;
-import com.sparta.billy.model.Reservation;
+import com.sparta.billy.model.*;
+import com.sparta.billy.repository.ReservationDateRepository;
 import com.sparta.billy.repository.ReservationQueryRepository;
 import com.sparta.billy.repository.ReservationRepository;
 import com.sparta.billy.util.Check;
@@ -18,6 +17,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -26,11 +30,12 @@ import java.util.List;
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final ReservationQueryRepository reservationQueryRepository;
+    private final ReservationDateRepository reservationDateRepository;
     private final Check check;
 
     @Transactional
     public ResponseDto<?> createReservation(ReservationRequestDto reservationRequestDto,
-                                            HttpServletRequest request) {
+                                            HttpServletRequest request) throws ParseException {
         Member billyMember = check.validateMember(request);
         Post post = check.getCurrentPost(reservationRequestDto.getPostId());
         check.checkPost(post);
@@ -46,6 +51,32 @@ public class ReservationService {
                 .endDate(reservationRequestDto.getEndDate())
                 .build();
         reservationRepository.save(reservation);
+
+        final String DATE_PATTERN = "yyyy-MM-dd";
+        String inputStartDate = reservationRequestDto.getStartDate();
+        String inputEndDate = reservationRequestDto.getEndDate();;
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_PATTERN);
+
+        Date startDate = sdf.parse(inputStartDate);
+        Date endDate = sdf.parse(inputEndDate);
+
+        List<String> dates = new ArrayList<>();
+        Date currentDate = startDate;
+        while (currentDate.compareTo(endDate) <= 0) {
+            dates.add(sdf.format(currentDate));
+            Calendar c = Calendar.getInstance();
+            c.setTime(currentDate);
+            c.add(Calendar.DAY_OF_MONTH, 1);
+            currentDate = c.getTime();
+        }
+
+        for (String date : dates) {
+            ReservationDate reservationDate = ReservationDate.builder()
+                    .reservationDate(date)
+                    .reservation(reservation)
+                    .build();
+            reservationDateRepository.save(reservationDate);
+        }
         return ResponseDto.success("예약이 접수되었습니다.");
     }
 
@@ -61,6 +92,9 @@ public class ReservationService {
         check.validateReservation(reservation);
         check.validateDelivery(reservationStateRequestDto.getState(), reservation);
 
+        if (reservationStateRequestDto.getState() == 3) {
+            reservationDateRepository.deleteAllByReservation(reservation);
+        }
         reservation.changeState(reservationStateRequestDto);
         return ResponseEntity.ok().body(SuccessDto.valueOf("true"));
     }
@@ -82,7 +116,7 @@ public class ReservationService {
         if (reservationStateRequestDto.getState() != 3) {
             reservationStateRequestDto.setState(3);
         }
-
+        reservationDateRepository.deleteAllByReservation(reservation);
         reservation.changeState(reservationStateRequestDto);
         return ResponseEntity.ok().body(SuccessDto.valueOf("true"));
     }
@@ -109,6 +143,14 @@ public class ReservationService {
         Member billy = check.validateMember(request);
         check.tokenCheck(request, billy);
         List<ReservationDetailResponseDto> response = reservationQueryRepository.findReservationByBillyAndState(billy, state);
+        return ResponseDto.success(response);
+    }
+
+    @Transactional
+    public ResponseDto<?> getReservationByJullyAndState(int state, HttpServletRequest request) {
+        Member jully = check.validateMember(request);
+        check.tokenCheck(request, jully);
+        List<ReservationDetailResponseDto> response = reservationQueryRepository.findReservationByJullyAndState(jully, state);
         return ResponseDto.success(response);
     }
 }
