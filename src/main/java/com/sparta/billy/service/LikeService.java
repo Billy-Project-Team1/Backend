52a1fba2373f612bfc5730f9d694c3f7028be09d
keembogeul <318qhtmf@naver.com>
2022-09-07 20:48:42 +0900

    feat: 게시글 찜하기 기능 / 내가 찜한 게시글 조회 기능 추가

--- src/main/java/com/sparta/billy/service/LikeService.java
+++ src/main/java/com/sparta/billy/service/LikeService.java
@@ -1,22 +1,26 @@
 package com.sparta.billy.service;
 
 import com.sparta.billy.dto.LikeRequestDto;
+import com.sparta.billy.dto.PostDto.PostResponseDto;
 import com.sparta.billy.dto.ResponseDto;
 import com.sparta.billy.model.Like;
 import com.sparta.billy.model.Member;
 import com.sparta.billy.model.Post;
 import com.sparta.billy.repository.LikeRepository;
+import com.sparta.billy.repository.PostQueryRepository;
 import com.sparta.billy.util.Check;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class LikeService {
     private final LikeRepository likeRepository;
+    private final PostQueryRepository postQueryRepository;
     private final Check check;
 
     @Transactional
@@ -37,6 +41,15 @@ public class LikeService {
             likeRepository.deleteById(findLike.getId());
             return ResponseDto.success("찜하기 취소!");
         }
+
+    }
+
+    @Transactional
+    public ResponseDto<?> getLikePostListByMember(HttpServletRequest request) {
+        Member member = check.validateMember(request);
+        check.tokenCheck(request, member);
+        List<PostResponseDto> response = postQueryRepository.findLikePostByMember(member);
+        return ResponseDto.success(response);
     }
 
 }
