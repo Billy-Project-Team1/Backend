bb785db42d37582a79e7132ce297df0eaae4a3a2
keembogeul <318qhtmf@naver.com>
2022-09-10 01:57:07 +0900

    🚧 WIP: 검색 기능 진행 중

---
+++ src/main/java/com/sparta/billy/dto/PostDto/SearchRequestDto.java
@@ -0,0 +1,10 @@
+package com.sparta.billy.dto.PostDto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class SearchRequestDto {
+    private String keyword;
+}
