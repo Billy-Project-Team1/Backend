4cfc9a160985182f254417a497e3541d477376f1
keembogeul <318qhtmf@naver.com>
2022-09-13 16:21:35 +0900

    ✨ 유저 프로필 조회의 예약 조회 시 예약 상태 별 카운트 표시 추가

--- src/main/java/com/sparta/billy/repository/ReservationRepository.java
+++ src/main/java/com/sparta/billy/repository/ReservationRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.billy.repository;
 
+import com.sparta.billy.model.Member;
 import com.sparta.billy.model.Post;
 import com.sparta.billy.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,7 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findAllByPost(Post post);
     int countByPost(Post post);
+    int countReservationByBillyAndState(Member billy, int state);
+    int countReservationByJullyAndState(Member jully, int state);
+    List<Reservation> findAllByBilly(Member billy);
 }
