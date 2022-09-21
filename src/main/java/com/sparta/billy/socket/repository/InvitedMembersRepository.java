f31cb69a62f154c4581934ad955f2c0c20442406
keembogeul <318qhtmf@naver.com>
2022-09-21 17:22:44 +0900

    🔨 refactor: 채팅 관련 코드 리팩토링

--- src/main/java/com/sparta/billy/socket/repository/InvitedMembersRepository.java
+++ src/main/java/com/sparta/billy/socket/repository/InvitedMembersRepository.java
@@ -4,14 +4,15 @@ import com.sparta.billy.socket.model.InvitedMembers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface InvitedMembersRepository extends JpaRepository<InvitedMembers, Long> {
 
     void deleteByMemberIdAndRoomId(Long memberId, String roomId);
-    boolean existsByMemberIdAndRoomId(Long member_id, String roomId);
+    boolean existsByMemberIdAndRoomId(Long memberId, String roomId);
+    Optional<InvitedMembers> findByMemberIdAndRoomId(Long memberId, String roomId);
     List<InvitedMembers> findAllByMemberId(Long memberId);
     List<InvitedMembers> findAllByRoomId(String roomId);
-
     List<InvitedMembers> findAllByMemberIdAndReadCheck(Long memberId, Boolean readCheck);
 
 }
